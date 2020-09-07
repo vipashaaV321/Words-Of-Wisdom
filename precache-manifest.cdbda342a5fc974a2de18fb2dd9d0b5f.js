fa6deeafcc1d84e54252ede5fa2bee7261c8ead5
vipasha <vips3201v@gmail.com>
2020-09-07 14:24:28 +0530

    Updates

---
+++ precache-manifest.cdbda342a5fc974a2de18fb2dd9d0b5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eea310e83830033173a50bacf6b041d4",
+    "url": "/Words-Of-Wisdom/index.html"
+  },
+  {
+    "revision": "fdf41436524d05ec2428",
+    "url": "/Words-Of-Wisdom/static/css/main.89580b15.chunk.css"
+  },
+  {
+    "revision": "f3318545e336aa14981a",
+    "url": "/Words-Of-Wisdom/static/js/2.564f1b91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Words-Of-Wisdom/static/js/2.564f1b91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdf41436524d05ec2428",
+    "url": "/Words-Of-Wisdom/static/js/main.5e98f3c9.chunk.js"
+  },
+  {
+    "revision": "bf9c095dbd66805161a7",
+    "url": "/Words-Of-Wisdom/static/js/runtime-main.cabddd43.js"
+  },
+  {
+    "revision": "7dfe006770c4bb6411af8a10a6a1fb4c",
+    "url": "/Words-Of-Wisdom/static/media/scenery_illustration.7dfe0067.gif"
+  }
+]);
